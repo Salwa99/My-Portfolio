f238b15ee91f48453160f8e5b203dcb40a8de39e
Salwa99 <s.ritage99@gmail.com>
2023-09-22 20:18:16 +0100

    Install react scroll

--- src/components/home/Introduction.jsx
+++ src/components/home/Introduction.jsx
@@ -3,10 +3,10 @@ import image from '../../assets/19879462_6174096.svg'
 
 const Introduction = () => {
   return (
-    <div className='sm:flex flex-row align-center gap-[100px] mt-[4rem]'>
+    <section  className='sm:flex flex-row align-center gap-[100px] mt-[4rem] border-b-2'>
       <h1 className='text-7xl'>Welcome to my Portfolio</h1>
       <img src={image} className="sm:w-[40%] " />
-    </div>
+    </section>
   );
 }
 
