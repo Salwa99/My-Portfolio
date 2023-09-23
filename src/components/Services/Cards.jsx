faf657dca06f9ae395cdd782cef1136810c1c791
Salwa99 <s.ritage99@gmail.com>
2023-09-23 09:53:13 +0100

    Add style to the cards

--- src/components/Services/Cards.jsx
+++ src/components/Services/Cards.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import {HiArrowRight} from 'react-icons/hi'
 
 const Cards = ({icon, title, desc}) => {
   return (
-    <div className="w-full h-80 shadow-shadowOne rounded-lg flex items-center bg-gradient-to-r from-gray-100 to-gray-200 group hover:bg-gradient-to-r hover:from-gray-100 hover:to-yellow-400 transition-colors duration-150">
-        <div>
-            <span className='text-5xl text-yellow-500'>{icon}</span>
+    <div className="w-full h-90 px-12 py-10 shadow-shadowOne rounded-lg flex items-center bg-gradient-to-r from-gray-100 to-gray-200 group hover:bg-gradient-to-r hover:from-gray-100 hover:to-yellow-400 transition-colors duration-150">
+      <div>
+        <div className="flex flex-col gap-8">
+          <div>
+            <span className="text-5xl text-yellow-500 pl-10">{icon}</span>
+          </div>
+
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-titleFont font-bold text-gray-700">
+              {title}
+            </h2>
+            <p className="text-base">{desc}</p>
+            <span className="text-2xl text-yellow-500">
+              <HiArrowRight />
+            </span>
+          </div>
         </div>
-        
-      <h2>{title}</h2>
-      <p>{desc}</p>
+      </div>
     </div>
   );
 }
