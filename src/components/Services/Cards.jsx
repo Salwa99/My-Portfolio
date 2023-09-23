aea9949d8ac6b4511b3b9fc46b731915e6f24d02
Salwa99 <s.ritage99@gmail.com>
2023-09-23 10:08:48 +0100

    Add a smooth transition to teh cards

--- src/components/Services/Cards.jsx
+++ src/components/Services/Cards.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import {HiArrowRight} from 'react-icons/hi'
+import React from "react";
+import { HiArrowRight } from "react-icons/hi";
 
-const Cards = ({icon, title, desc}) => {
+const Cards = ({ icon, title, desc }) => {
   return (
-    <div className="w-full h-90 px-12 py-10 shadow-shadowOne rounded-lg flex items-center bg-gradient-to-r from-gray-100 to-gray-200 group hover:bg-gradient-to-r hover:from-gray-100 hover:to-yellow-400 transition-colors duration-150">
-      <div>
-        <div className="flex flex-col gap-8">
-          <div>
-            <span className="text-5xl text-yellow-500 pl-10">{icon}</span>
-          </div>
+    <div className="w-full h-82 px-12 py-10 shadow-shadowOne rounded-lg flex items-center bg-gradient-to-r from-gray-100 to-gray-200 group hover:bg-gradient-to-r hover:from-gray-100 hover:to-yellow-400 transition-colors duration-300 hover:transform hover:-translate-y-2 hover:transition-transform ease-in-out hover:duration-500">
+      <div className="flex flex-col gap-8">
+        <div>
+          <span className="text-5xl text-yellow-500 pl-10">{icon}</span>
+        </div>
 
-          <div className="flex flex-col gap-6">
-            <h2 className="text-2xl font-titleFont font-bold text-gray-700">
-              {title}
-            </h2>
-            <p className="text-base">{desc}</p>
-            <span className="text-2xl text-yellow-500">
-              <HiArrowRight />
-            </span>
-          </div>
+        <div className="flex flex-col gap-6">
+          <h2 className="text-2xl font-titleFont font-bold text-gray-700">
+            {title}
+          </h2>
+          <p className="text-base">{desc}</p>
+          <span className="text-2xl text-yellow-500">
+            <HiArrowRight />
+          </span>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Cards
+export default Cards;
