7b0975d14eca04acbf227c9bd6cb52ee980bd565
Salwa99 <s.ritage99@gmail.com>
2023-10-05 14:56:30 +0100

    Change the bg color f the project cards

--- src/components/projects/Cards.jsx
+++ src/components/projects/Cards.jsx
@@ -10,20 +10,18 @@ const Cards = () => {
         {ProjectsData.map((data) => (
           <div
             key={data._id}
-            className="w-full h-auto shadow-shadowOne flex flex-col bg-gradient-to-r from-[#f2f6fa]  to-[#8e9092] group hover:bg-gradient-to-b hover:from-yellow-100 hover:to-yellow-400 hover:border-2 hover:border-yellow-400 relative overflow-hidden bg-white rounded-lg group transform hover:-translate-y-2 transition-transform duration-300"
+            className="w-full h-auto shadow-shadowOne flex flex-col bg-[#000026] group hover:border-2 hover:border-[#F2C832] relative overflow-hidden rounded-lg group transform hover:-translate-y-2 transition-transform duration-300"
           >
             <div className="h-45 lg:h-70 bg-center bg-cover">
               <img src={data.src} alt="project image" />
             </div>
             <div className="px-6 py-4">
-              <h2 className="title text-xl font-extrabold text-gray-800 uppercase mb-2 relative">
+              <h2 className="title text-xl font-extrabold text-gray-300 uppercase mb-2 relative">
                 {data.title}
                 <span className="absolute top-1/2 right-0 transform -translate-y-1/2 w-8 h-5 bg-no-repeat bg-top"></span>
               </h2>
-              <p className="text-sm text-gray-600 mb-4">
-                {data.description}
-              </p>
-              <h3 className="text-4xl flex gap-4 uppercase text-gray-500 cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <p className="text-sm text-gray-100 mb-4">{data.description}</p>
+              <h3 className="text-4xl flex gap-4 uppercase text-gray-300 cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 <a href={data.live} target="_blank" rel="noopener noreferrer">
                   <GoLinkExternal />
                 </a>{" "}
